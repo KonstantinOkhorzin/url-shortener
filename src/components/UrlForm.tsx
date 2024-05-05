0a2be88bd40569767f7f572796bd2ff8e435b115
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-05-05 08:53:18 +0300

    feat: add UrlForm component

---
+++ src/components/UrlForm.tsx
@@ -0,0 +1,76 @@
+import { FC, useEffect } from 'react';
+import { TextField, Button, Box, Typography, CircularProgress } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import { Status } from '../types';
+
+interface Props {
+  setFullUrl: (text: string) => void;
+  status: Status;
+}
+
+interface FormValues {
+  url: string;
+}
+
+const schema = yup
+  .object({
+    url: yup.string().url('URL is invalid').required('URL is empty!'),
+  })
+  .required();
+
+const UrlForm: FC<Props> = ({ setFullUrl, status }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FormValues) => setFullUrl(data.url);
+
+  useEffect(() => {
+    if (isSubmitSuccessful && status === Status.FULFILLED) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset, status]);
+
+  return (
+    <Box
+      component='form'
+      onSubmit={handleSubmit(onSubmit)}
+      noValidate
+      autoComplete='off'
+      sx={{ display: 'grid', gridTemplateColumns: '1fr 98px', position: 'relative' }}
+    >
+      <TextField
+        autoFocus
+        type='url'
+        {...register('url')}
+        variant='outlined'
+        placeholder='Original URL'
+        sx={{ background: 'white' }}
+      />
+      <Button
+        type='submit'
+        variant='contained'
+        color='success'
+        disabled={status === Status.PENDING}
+      >
+        {status === Status.PENDING ? <CircularProgress size={24} /> : 'Shorten'}
+      </Button>
+
+      {errors.url && (
+        <Typography color='error' sx={{ position: 'absolute', bottom: '-35px', left: '14px' }}>
+          {errors.url.message}
+        </Typography>
+      )}
+    </Box>
+  );
+};
+
+export default UrlForm;
