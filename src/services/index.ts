0a5f1215aa3d012f374102bdafe65df5b27fde71
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-05-05 07:28:19 +0300

    feat: add types

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+interface SuccessResponse {
+  result_url: string;
+}
+
 const apiUrl = 'https://cleanuri-proxy-server.onrender.com/api';
 
 export const getShortenedURL = async (url: string) => {
   try {
-    return (await axios.post(apiUrl, { url })).data;
+    return (await axios.post<SuccessResponse>(apiUrl, { url })).data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       return Promise.reject(error.message);
